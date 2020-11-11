2cfe8751864329f6bbb280dd4dd9fc51e05c428c
Daysired De Jesus <daisy@Daysireds-MBP.home>
2020-11-10 20:37:33 -0500

    CSS Changes

---
+++ app.js
@@ -0,0 +1,12 @@
+// const DOMAIN = 'http://superheroapi.com/';
+// const API_KEY = '1743621192472664'
+
+// async function getHeroId(id) {
+//   // const url = 'https://www.superheroapi.com/api.php/1743621192472664/search/id'
+
+//   try {
+//     const response = await axios.get(url)
+//     const search = response.data.Search
+//     const heroId = document.querySelector('hero-id')
+//   }
+// }

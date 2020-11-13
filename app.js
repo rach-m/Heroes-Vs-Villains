7bf04bce470759b37e886ace4bceb98350719be0
Daysired De Jesus <daisy@Daysireds-MBP.home>
2020-11-13 08:39:56 -0500

    ordered-hero-info

--- app.js
+++ app.js
@@ -15,26 +15,42 @@ async function getHeroId(id) {
     console.log(search)
 
     search.forEach((item) => {
-      const heroId = document.querySelector('.hero-id')
-      const idDiv = document.createElement('div')
-      idDiv.className = 'id-div'
-      heroId.append(idDiv)
-
-
-      const info = document.createElement('p')
-      
-      const appearance = item.appearance['gender'] + item.appearance['race'] + item.appearance['height'] + item.appearance['weight'] + item.appearance['eye-color']
-      const biography = item.biography['full-name'] + item.biography['aliases'] + item.biography['place-of-birth']
-      const image = item.image
-      const powerstats = item.powerstats['intelligence'] + item.powerstats['strength'] + item.powerstats['speed'] + item.powerstats['durability'] + item.powerstats['power']
-      const work = item.work['occupation'] + item.work['base']
-      const connections = item.connections['group-affiliation']
-      info.innerHTML = `${biography} ${powerstats} ${appearance} ${work} ${connections} ${image}`
-      idDiv.append(info)
-      
+      const heroCard = document.querySelector('.hero-id')
       const img = document.createElement('img')
       img.setAttribute('src', item.image.url)
-      idDiv.append(img)
+      heroCard.append(img)
+
+      const info = document.createElement('p')
+      // const appearance = item.appearance['gender'] + item.appearance['race'] + item.appearance['height'] + item.appearance['weight'] + item.appearance['eye-color']
+      const appearance = item.appearance
+      const appearanceSection = document.createElement('section')
+      const appearanceH2 = document.createElement('h2')
+      appearanceH2.innerText = "appearance"
+      console.log(appearance)
+      const appearanceUl = document.createElement('ul')
+      for (let key in appearance) {
+        const item = document.createElement('li')
+        item.innerText = `${key}: ${appearance[key]}`
+        appearanceUl.append(item)
+      }
+      appearanceSection.append(appearanceH2)
+      appearanceSection.append(appearanceUl)
+      heroCard.append(appearanceSection)
+
+      // const biography = item.biography['full-name'] + item.biography['aliases'] + item.biography['place-of-birth']
+      // con
+
+      // const image = item.image
+
+      // const powerstats = item.powerstats['intelligence'] + item.powerstats['strength'] + item.powerstats['speed'] + item.powerstats['durability'] + item.powerstats['power']
+
+      // const work = item.work['occupation'] + item.work['base']
+
+      // const connections = item.connections['group-affiliation']
+
+      // info.innerHTML = `${biography} ${powerstats} ${appearance} ${work} ${connections} ${image}`
+      // heroCard.append(info)
+      
 
     });
   } catch (error) {
